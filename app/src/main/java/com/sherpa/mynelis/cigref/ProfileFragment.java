433f78ede761caa77d74124e86be8ba6753f6108
Alexis Largaiolli <alexis.largaiolli.pro@gmail.com>
2017-10-17 14:30:04 +0200

    Login form

--- app/src/main/java/com/sherpa/mynelis/cigref/ProfileFragment.java
+++ app/src/main/java/com/sherpa/mynelis/cigref/ProfileFragment.java
@@ -3,9 +3,13 @@ package com.sherpa.mynelis.cigref;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.sherpa.mynelis.cigref.events.EventAdpader;
 
 
 /**
@@ -13,6 +17,11 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+
+    private RecyclerView mRecyclerView;
+    private EventAdpader mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = new String[]{"item1", "item2", "item3"};
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -22,8 +31,23 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.event_recycler_view_profil);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(container.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new EventAdpader(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
     }
 
 }
--- app/src/main/java/com/sherpa/mynelis/cigref/events/EventByPopularityFragment.java
+++ app/src/main/java/com/sherpa/mynelis/cigref/events/EventByPopularityFragment.java
@@ -4,6 +4,8 @@ package com.sherpa.mynelis.cigref.events;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,10 @@ import com.sherpa.mynelis.cigref.R;
  */
 public class EventByPopularityFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private EventAdpader mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = new String[]{"item1", "item2", "item3"};
 
     public EventByPopularityFragment() {
         // Required empty public constructor
@@ -25,8 +31,23 @@ public class EventByPopularityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_by_popularity, container, false);
+        View view =  inflater.inflate(R.layout.fragment_event_by_popularity, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.event_recycler_view_by_pop);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(container.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new EventAdpader(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
     }
 
 }
